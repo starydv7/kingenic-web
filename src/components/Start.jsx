85fa109e5acd32503480d7fc9f262341d8a3e875
starydv7 <55851664+starydv7@users.noreply.github.com>
2023-09-11 19:21:49 +0530

    web update

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import style from '../css/start.module.css';
+import logo from "../assets/biglogo.png";
 const Start = () => {
     
   return (
     <div className={style.main}>
-    <div>
-      
+    <div className={style.content}>
+      <div className={style.contentImage}>
+        <div className={style.logoContainer}>
+        <img src={logo}alt="logo" className={style.logoImg}/>
+        <p className={style.text}>The new era of intelligent <b>nutrition</b> is here</p>
+        </div>
+        <div className={style.buttonContainer}>
+              <button className={style.button}>Get Started</button>
+            </div>
+      </div>
     </div>
       
     </div>
