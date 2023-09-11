682d0353a0af0dc1e75c50a2059f2f13fe114a95
starydv7 <55851664+starydv7@users.noreply.github.com>
2023-09-11 13:43:20 +0530

    home page

---
+++ src/components/Start.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import style from '../css/start.module.css';
+const Start = () => {
+    
+  return (
+    <div className={style.main}>
+    <div>
+      
+    </div>
+      
+    </div>
+  );
+};
+
+export default Start;
