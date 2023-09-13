872c1753a77e837a2364b7314a7a4908a283f327
starydv7 <55851664+starydv7@users.noreply.github.com>
2023-09-13 23:58:22 +0530

    update Home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,60 @@
 import React from 'react'
 import style from "../css/home.module.css";
+import arrow from "../assets/arrow.png";
+import logo from "../assets/biglogo.png";
+import banner from "../assets/image1.png";
+import card1 from "../assets/card1.png";
+import card2 from "../assets/card2.png";
+import card3 from "../assets/card3.png";
 const Home = () => {
   return (
     <div className={style.maindiv}>
         <div className={style.navbardiv}>
-         <div className={style.arrow}></div>
-         <div className={style.logodiv}></div>
+         <div className={style.arrow}>
+        <img src={arrow}alt="arrow" className={style.arrowImg}/>
+         </div>
+         <div className={style.logodiv}>
+<img src={logo}alt="logo"className={style.logoImg}/>
+         </div>
         </div>
         <div className={style.line}></div>
      <div className={style.bannerdiv}>
-      
+      <img src={banner}alt="banner"className={style.bannerImg}/>
      </div>
      <div className={style.cardDiv}>
-        <div className={style.gridCard}></div>
-        <div className={style.gridCard}></div>
-        <div className={style.gridCard}></div>
+        <div className={style.gridCard}>
+          <div className={style.gridImg}>
+            <img src={card1}alt="card1"className={style.cardimg}/>
+          </div>
+          <div className={style.gridText}>
+            <p className={style.gridTitle}>Product Catalog</p>
+            <p className={style.subTitle}>
+            Explore our branded portfolio and private label offerings
+            </p>
+          </div>
+        </div>
+        <div className={style.gridCard}>
+        <div className={style.gridImg}>
+         <img src={card2}alt="card1"className={style.cardimg}/>
+         </div>
+          <div className={style.gridText}>
+          <p className={style.gridTitle}>Customer Portal</p>
+            <p className={style.subTitle}>
+            From documentation to live service portal
+            </p>
+          </div>
+        </div>
+        <div className={style.gridCard}>
+        <div className={style.gridImg}>
+        <img src={card3}alt="card1"className={style.cardimg}/>
+        </div>
+          <div className={style.gridText}>
+          <p className={style.gridTitle}>Learning Center</p>
+            <p className={style.subTitle}>
+            Access free, expert-created education based on your interests and needs.
+            </p>
+          </div>
+        </div>
      </div>
     </div>
   )
